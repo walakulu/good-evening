7a32dcbf61a7a4add1a6c22675917f57ada35ebb
Hasitha Maduranga <maduranga.bbnt@gmail.com>
2021-03-21 13:53:19 +0530

    initial commit

---
+++ src/main/java/com/demo/microservice/goodevening/GoodEveningApplication.java
@@ -0,0 +1,13 @@
+package com.demo.microservice.goodevening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodEveningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodEveningApplication.class, args);
+	}
+
+}
